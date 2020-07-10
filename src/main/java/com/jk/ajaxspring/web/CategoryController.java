9c9642d397e6ae0dfe59cf8a422227e8ba7b5008
Janakiram Sampalli <janakiram.sampalli@gmail.com>
2020-07-10 10:03:02 +1000

    intial commit

---
+++ src/main/java/com/jk/ajaxspring/web/CategoryController.java
@@ -0,0 +1,35 @@
+package com.jk.ajaxspring.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jk.ajaxspring.model.Category;
+import com.jk.ajaxspring.model.CategoryRepository;
+
+@Controller
+public class CategoryController {
+
+	@Autowired
+	private CategoryRepository categoryRepo;
+
+	@GetMapping("/")
+	public String home() {
+		//Return the default home page.
+		return "index";
+	}
+
+	@RequestMapping(path = "/listajax", produces = "application/json; charset=UTF-8")
+	@ResponseBody
+	public List<Category> findCities() {
+
+		//Load all cities and return as JSON response.
+		List<Category> cities = categoryRepo.findAll();
+		return cities;
+	}
+	
+}
---
+++ src/test/java/com/jk/ajaxspring/AjaxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.ajaxspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
